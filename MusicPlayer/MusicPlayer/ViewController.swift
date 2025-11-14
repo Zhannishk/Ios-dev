6891b0554da5287c025875a61786b3d0b4ee4ac2
Zhannishk <janiaa.uk@gmail.com>
2025-11-14 22:13:52 +0500

    final_with_requiremnts

--- MusicPlayer/MusicPlayer/ViewController.swift
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -10,43 +10,42 @@ import AVFoundation
 
 class ViewController: UIViewController {
     var player: AVAudioPlayer?
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        songName.text = musics[constant]
-        songArtits.text = artists[constant]
-        songCover.image = UIImage(named: "\(constant+1)")
-        playSound(name: musics[0])
-        // Do any additional setup after loading the view.
+        displaySong(song: 0)
+        player?.stop()
     }
     
     let musics = ["Anlasana", "The Last Waltz", "Criminal", "Medieval", "The Less I Know The Better"]
     let artists = ["Nahide Babashli", "Cho Young-Wuk", "Britney Spears", "FINNEAS", "Tame Impala"]
     
     var constant = 0
-    var range = 5
+    var range: Int {
+        return musics.count
+    }
     
     @IBOutlet weak var songCover: UIImageView!
     @IBOutlet weak var songName: UILabel!
     @IBOutlet weak var songArtits: UILabel!
     
     @IBAction func play(_ sender: UIButton) {
-        songName.text = musics[constant]
-        songArtits.text = artists[constant]
-        songCover.image = UIImage(named: "\(constant+1)")
-        playSound(name: musics[constant])
+        guard let player = player else { return }
+
+        if player.isPlaying {
+            player.pause()
+        } else {
+            player.play()
+        }
     }
-    
     
     @IBAction func prev(_ sender: UIButton) {
         constant -= 1
         if constant < 0 {
             constant = range - 1
         }
-        songName.text = musics[constant]
-        songArtits.text = artists[constant]
-        songCover.image = UIImage(named: "\(constant+1)")
-        playSound(name: musics[constant])
+        displaySong(song: constant)
     }
     
     @IBAction func next(_ sender: UIButton) {
@@ -54,20 +53,23 @@ class ViewController: UIViewController {
         if constant >= range {
             constant = 0
         }
-        songName.text = musics[constant]
-        songArtits.text = artists[constant]
-        songCover.image = UIImage(named: "\(constant+1)")
-        playSound(name: musics[constant])
+        displaySong(song: constant)
     }
     
     
-    private func playSound(name: String){
+    func playSound(name: String){
         guard let url = Bundle.main.url(forResource: name, withExtension: "m4a") else {
             return
         }
         player = try? AVAudioPlayer(contentsOf: url)
         player?.play()
-        
+    }
+    
+    func displaySong(song: Int){
+        songName.text = musics[constant]
+        songArtits.text = artists[constant]
+        songCover.image = UIImage(named: "\(constant+1)")
+        playSound(name: musics[constant])
     }
 
     
